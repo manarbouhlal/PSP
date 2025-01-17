0a2533febef11232a883f2dc15c2a0f9992e2572
manarbouhlal <166365994+manarbouhlal@users.noreply.github.com>
2025-01-17 12:33:07 +0100

    Add files via upload

---
+++ PSP/MultiThreadAstronauntsWalking.java
@@ -0,0 +1,21 @@
+package PSP;
+
+public class MultiThreadAstronauntsWalking {
+
+	public static void main(String[] args) {
+		// Create threads
+		Astronaunt astronaunt1 = new Astronaunt ("Manolo",2) ;
+		Astronaunt astronaunt2 = new Astronaunt ("Pepe",3) ;
+		Astronaunt astronaunt3 = new Astronaunt ("Manuel",1) ;
+		
+		// Start the threads
+		astronaunt1.start();
+		astronaunt2.start();
+		astronaunt3.start();
+		
+		// Main threads continues
+		System.out.println("All pets are eating...");
+		
+	}
+
+}
